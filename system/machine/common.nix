38f048103f9b40e891205d9b086cd3793dd88259
Leon Breedt <leon@sector42.io>
2022-11-01 09:08:34 +1300

    fix: disable firewall, add SSH to common.nix

--- system/machine/common.nix
+++ system/machine/common.nix
@@ -106,4 +106,21 @@
     # if the clipboard sill works.
     gtkmm3
   ];
+
+  # Enable OpenSSH
+  services.openssh.enable = true;
+  services.openssh.passwordAuthentication = true;
+  services.openssh.permitRootLogin = "no";
+
+  # Disable the firewall since we're in a VM and we want to make it
+  # easy to visit stuff in here. We only use NAT networking anyways.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05";
 }
