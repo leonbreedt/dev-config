cb21b758c7091859d0096299288e7de436157a3a
Leon Breedt <leon@sector42.io>
2022-11-11 14:25:39 +1300

    fix: add /boot filesystem

--- system/machine/hw-x64.nix
+++ system/machine/hw-x64.nix
@@ -18,6 +18,10 @@
     { device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
     };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
 
   swapDevices =
     [ { device = "/dev/disk/by-label/swap"; }
