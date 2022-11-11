42e933ac1185435b9b74343480dd30f6a19edc52
Leon Breedt <leon@sector42.io>
2022-11-11 17:17:46 +1300

    feat: add DisplayPort monitors to bspwm configuration

--- users/leon/home.nix
+++ users/leon/home.nix
@@ -45,6 +45,8 @@
     enable = true;
     monitors = {
       "Virtual-1" = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" ];
+      "DP-0" = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" ];
+      "DP-4" = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" ];
     };
     alwaysResetDesktops = true;
     settings = {
